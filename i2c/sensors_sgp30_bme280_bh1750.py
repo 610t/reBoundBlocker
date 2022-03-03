681c5ea66262806e53c1777dcb3dc0d53c198852
Takeshi MUTOH <takeshi.muoth@gmail.com>
2022-03-03 20:19:03 +0900

    Show sensor's values: - Light sensor: BH1750 - Environment sensor: BME280 - eCO2&TVOC sensor: SGP30

---
+++ i2c/sensors_sgp30_bme280_bh1750.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import smbus
+import time
+import subprocess
+from subprocess import PIPE
+
+addr=0x58
+
+i2c=smbus.SMBus(1)
+i2c.write_byte_data(addr, 0x36, 0x82)
+time.sleep(0.01)
+i2c.write_byte_data(addr, 0x20, 0x2f)
+time.sleep(0.01)
+i2c.write_byte_data(addr, 0x20, 0x03)
+time.sleep(0.01)
+#time.sleep(15)
+
+while True:
+  ## BH1750
+  proc_bh1750=subprocess.run("/home/pi/reBoundBlocker/i2c/bh1750.py",shell=True,stdout=PIPE,text=True)
+  ## BME280
+  proc_bme280=subprocess.run("/home/pi/reBoundBlocker/i2c/bme280.py",shell=True,stdout=PIPE,text=True)
+  ## SGP30
+  i2c.write_byte_data(addr, 0x20, 0x08)
+  time.sleep(0.01)
+  data=i2c.read_i2c_block_data(addr,0x00,6)
+  time.sleep(0.01)
+  #print("Raw:",data)
+
+  # DISPLAY result
+  print(proc_bh1750.stdout.strip())
+  print(proc_bme280.stdout.strip())
+  print("eCO2:",data[0]<<8|data[1], flush=True)
+  print("TVOC:",data[3]<<8|data[4], flush=True)
+  # time.sleep(1)
