d74a8c11fbea7ae84e89649740b3353d7731f96c
Takeshi MUTOH <takeshi.muoth@gmail.com>
2022-03-11 10:02:47 +0900

    Add video playback time handling.

--- URL_changer/URL_changer.py
+++ URL_changer/URL_changer.py
@@ -20,10 +20,15 @@ path=os.path.dirname(__file__)
 tmpl_file=path+"/iframe.html.tmpl"
 output_file=path+"/iframe.html"
 
+### Get movie duration from script ../YouTube/youtube_duration.py.
+# Please see a code at RasPiCon2018 at ~/pychromecast/examples/my_youtube.py.
+duration=int(subprocess.check_output(path+"/../YouTube/youtube_duration.py --id "+movie_list[v], shell=True))
+duration=int((duration+10)*1.1)
+
 with open(tmpl_file) as f_tmpl:
   tmpl=f_tmpl.read()
   tmp=tmpl.replace('%%%URL%%%',url)
-  out=tmp.replace('%%%TIME%%%',str(random.randint(1,1000)))
+  out=tmp.replace('%%%TIME%%%',str(duration))
   with open(output_file, mode='w') as f_out:
     f_out.write(out)
 
@@ -31,3 +36,4 @@ time.sleep(5)
 
 # Open browser on new window.
 subprocess.run(path+"/../bin/create_new_browser.sh "+path+"/iframe.html", shell=True)
+time.sleep(duration)
