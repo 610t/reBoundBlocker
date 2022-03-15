62c6c38d43fdfff92d74089d046779550eaea7ad
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2022-03-15 18:41:33 +0900

    Change light sensor from BH1750 to reTerminal internal sensor LTR-303ALS-01. Wait 1 sec to avoid "Remote I/O error".

--- i2c/all_sensors.py
+++ i2c/all_sensors.py
@@ -19,11 +19,11 @@ time.sleep(0.01)
 path=os.path.dirname(__file__)
 
 while True:
-  ## BH1750
-  proc_bh1750=subprocess.run(path+"/bh1750.py",shell=True,stdout=PIPE,text=True)
-  ## BME280
-  proc_bme280=subprocess.run(path+"/bme280.py",shell=True,stdout=PIPE,text=True)
-  ## SGP30
+  ## Light sensor:	LTR-303ALS-01
+  proc_light=subprocess.run(path+"/ltr-303als-01.sh",shell=True,stdout=PIPE,text=True)
+  ## Environment sensor:	BME280
+  proc_env=subprocess.run(path+"/bme280.py",shell=True,stdout=PIPE,text=True)
+  ## CO2 sensor:	SGP30
   i2c.write_byte_data(addr, 0x20, 0x08)
   time.sleep(0.01)
   data=i2c.read_i2c_block_data(addr,0x00,6)
@@ -31,8 +31,8 @@ while True:
   #print("Raw:",data)
 
   # DISPLAY result
-  print(proc_bh1750.stdout.strip())
-  print(proc_bme280.stdout.strip())
+  print(proc_light.stdout.strip())
+  print(proc_env.stdout.strip())
   print("eCO2:",data[0]<<8|data[1], flush=True)
   print("TVOC:",data[3]<<8|data[4], flush=True)
-  # time.sleep(1)
+  time.sleep(1)
