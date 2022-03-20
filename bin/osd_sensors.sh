35cb59b5b819da8fff72ba593ca364facf186a6c
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2022-03-20 16:23:06 +0900

    Format with VSCode formatter.

--- bin/osd_sensors.sh
+++ bin/osd_sensors.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 export DISPLAY=:0
-MY_PATH=`dirname $0`
-while true
-do
+MY_PATH=$(dirname $0)
+while true; do
   ${MY_PATH}/../i2c/all_sensors.py
-done | \
-osd_cat --align left \
-  --pos bottom \
-  --lines 6 \
-  -c white \
-  --font -*-*-*-*-*--34-*-*-*-*-*-*-* \
-  -s 5
-
-  #-delay 1 \
+done |
+  osd_cat --align left \
+    --pos bottom \
+    --lines 6 \
+    -c white \
+    --font -*-*-*-*-*--34-*-*-*-*-*-*-* \
+    -s 5
+ß
--- bin/osd_steps.sh
+++ bin/osd_steps.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 export DISPLAY=:0
-MY_PATH=`dirname $0`
-while true
-do
+MY_PATH=$(dirname $0)
+while true; do
   sudo ${MY_PATH}/../BLE/Get_from_M5Walker.py
   sleep 10
-done | \
-osd_cat --align right \
-  --pos bottom \
-  --lines 1 \
-  -c white \
-  --font -*-*-*-*-*--34-*-*-*-*-*-*-* \
-  -d 30 \
-  -s 5
-
+done |
+  osd_cat --align right \
+    --pos bottom \
+    --lines 1 \
+    -c whiteß \
+    --font -*-*-*-*-*--34-*-*-*-*-*-*-* \
+    -d 30 \
+    -s 5
