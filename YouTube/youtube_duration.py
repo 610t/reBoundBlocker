56a4b7dfd540dbf5327ef4646a8c6bec105dbf74
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2022-03-20 16:14:53 +0900

    Format with VSCode formatter.

--- YouTube/youtube_duration.py
+++ YouTube/youtube_duration.py
@@ -13,40 +13,42 @@ DEVELOPER_KEY = "MY_DEVELOPER_KEY"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def youtube_search_video(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
 
-  videos_response = youtube.videos().list(
-    part="contentDetails,status",
-    id=options.id
-  ).execute()
+def youtube_search_video(options):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    videos_response = youtube.videos().list(
+        part="contentDetails,status",
+        id=options.id
+    ).execute()
 
 #  videos = []
 
-  for result in videos_response.get("items", []):
-    duration = result["contentDetails"]["duration"]
-    # This video is embeddable?
-    embeddable = result["status"]["embeddable"]
-    if not embeddable:
-      print(-1)
-      exit()
-    pattern = r'PT(?:(?P<hours>\d+)H)?((?P<minutes>\d+)M)?(?P<seconds>\d+)S'
-    match = re.match(pattern, duration)
+    for result in videos_response.get("items", []):
+        duration = result["contentDetails"]["duration"]
+        # This video is embeddable?
+        embeddable = result["status"]["embeddable"]
+        if not embeddable:
+            print(-1)
+            exit()
+        pattern = r'PT(?:(?P<hours>\d+)H)?((?P<minutes>\d+)M)?(?P<seconds>\d+)S'
+        match = re.match(pattern, duration)
 
-    hours = int(match.group('hours') or 0)
-    minutes = int(match.group('minutes') or 0)
-    seconds = int(match.group('seconds') or 0)
+        hours = int(match.group('hours') or 0)
+        minutes = int(match.group('minutes') or 0)
+        seconds = int(match.group('seconds') or 0)
 #    print(duration, hours, minutes, seconds)
-    print(60*60*hours+60*minutes+seconds)
+        print(60*60*hours+60*minutes+seconds)
 #    videos.append("%s" % (duration))
 #  print("\n".join(videos), "\n")
 
-if __name__ == "__main__":
-  argparser.add_argument("--id", help="VideoIDs", default="Sq5QW3YqipM")
-  args = argparser.parse_args()
 
-  try:
-    youtube_search_video(args)
-  except HttpError as e:
-    print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
+if __name__ == "__main__":
+    argparser.add_argument("--id", help="VideoIDs", default="Sq5QW3YqipM")
+    args = argparser.parse_args()
+
+    try:
+        youtube_search_video(args)
+    except HttpError as e:
+        print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
--- YouTube/youtube_duration_player.py
+++ YouTube/youtube_duration_player.py
@@ -5,26 +5,28 @@ import json
 from pytube.innertube import InnerTube
 from oauth2client.tools import argparser
 
-def youtube_search_video(options):
-  id=options.id
-  innertube = InnerTube()
-  video_info = innertube.player(id)
 
-  duration = video_info["streamingData"]["formats"][0]["approxDurationMs"]
-  # This video is embeddable?
-  embeddable = video_info["playabilityStatus"]["playableInEmbed"]
-  if not embeddable:
-    print(-1)
-    exit()
-  else:
-    print(int(duration)/1000)
-    # print(embeddable)
+def youtube_search_video(options):
+    id = options.id
+    innertube = InnerTube()
+    video_info = innertube.player(id)
+
+    duration = video_info["streamingData"]["formats"][0]["approxDurationMs"]
+    # This video is embeddable?
+    embeddable = video_info["playabilityStatus"]["playableInEmbed"]
+    if not embeddable:
+        print(-1)
+        exit()
+    else:
+        print(int(duration)/1000)
+        # print(embeddable)
+
 
 if __name__ == "__main__":
-  argparser.add_argument("--id", help="VideoIDs", default="Sq5QW3YqipM")
-  args = argparser.parse_args()
+    argparser.add_argument("--id", help="VideoIDs", default="Sq5QW3YqipM")
+    args = argparser.parse_args()
 
-  try:
-    youtube_search_video(args)
-  except HttpError as e:
-    print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
+    try:
+        youtube_search_video(args)
+    except HttpError as e:
+        print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
